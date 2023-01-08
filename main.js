b8418614499c8097a7b9cde706996e421b88ef92
Madhav Jha <82491455+madhavjha97@users.noreply.github.com>
2023-01-08 15:48:23 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,11 @@
+function currencyConvert() {
+    let inr = document.getElementById('inr').value;
+
+
+    let convert = (inr * 80);
+
+    document.getElementById('result').value = convert;
+}
+
+
+
